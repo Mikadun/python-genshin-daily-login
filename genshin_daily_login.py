104faf520fd6cda15fe2f2500dd3b58ab66f160c
Mikadun <gribak98@list.ru>
2022-09-06 18:59:47 +1000

    Proper exception handling if driver gets old

--- genshin_daily_login.py
+++ genshin_daily_login.py
@@ -2,49 +2,49 @@ import os
 import pickle
 import time
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, InvalidCookieDomainException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, InvalidCookieDomainException, \
+    SessionNotCreatedException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_auto_update import check_driver
 
-def genshin_daily_login(headless = True, force_login = False):
 
+def genshin_daily_login(headless=True, force_login=False):
     # Setup chrome driver parameters
-    PATH_TO_DRIVER = './chromedriver.exe'
-    PATH_TO_COOKIES = './cookies.pkl'
+    path_to_driver = './chromedriver.exe'
+    path_to_cookies = './cookies.pkl'
     options = Options()
     options.add_argument('--log-level=3')
 
     # Show chrome if user need to manually log it
     options.headless = headless
 
-    if not os.path.isfile(PATH_TO_COOKIES):
+    if not os.path.isfile(path_to_cookies) or force_login:
         options.headless = False
 
-    service = Service(PATH_TO_DRIVER)
+    service = Service(path_to_driver)
 
     try:
         driver = webdriver.Chrome(service=service, options=options)
-    except:
+    except SessionNotCreatedException:
         check_driver('./')
         driver = webdriver.Chrome(service=service, options=options)
 
 
     # preload page to evade cookies domain error
     url = 'https://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=en-us'
-    driver.get(url)
 
+    driver.get(url)
+    
     # check if cookies exists
-    # if not then let user log in and save cookies
-    if not os.path.isfile(PATH_TO_COOKIES) or force_login:
+    # and if is not then let user log in and save cookies
+    if not os.path.isfile(path_to_cookies) or force_login:
         input('Log in and then press enter')
-        pickle.dump(driver.get_cookies() , open(PATH_TO_COOKIES, 'wb'))
+        pickle.dump(driver.get_cookies(), open(path_to_cookies, 'wb'))
 
     # load cookies and add them to driver
-    cookies = pickle.load(open(PATH_TO_COOKIES, 'rb'))
+    cookies = pickle.load(open(path_to_cookies, 'rb'))
     for cookie in cookies:
         driver.add_cookie(cookie)
 
@@ -60,25 +60,22 @@ def genshin_daily_login(headless = True, force_login = False):
         reward = driver.find_element(By.CSS_SELECTOR, "div[class*='---active---']")
 
         driver.execute_script("arguments[0].click();", reward)
-        
+
         print('Claimed reward')
     except NoSuchElementException:
         print('No reward to claim')
 
     # save cookies
-    pickle.dump(driver.get_cookies() , open(PATH_TO_COOKIES, 'wb'))
+    pickle.dump(driver.get_cookies(), open(path_to_cookies, 'wb'))
 
     time.sleep(1)
 
     driver.close()
 
+
 if __name__ == '__main__':
     try:
         genshin_daily_login()
-    # this error might happen when opening in headless mode
-    except ElementClickInterceptedException:
-        print('Got intercepted')
-        genshin_daily_login(headless = False)
     # this error might happen when cookies get old or site changes
     except InvalidCookieDomainException:
-        genshin_daily_login(headless = False, force_login = True)
+        genshin_daily_login(force_login=True)
